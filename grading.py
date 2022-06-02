a6fc21809e4e28be6e05aa268abe6178935d0c33
Dagne16 <dagnachewtilahun16@gmail.com>
2022-06-02 21:14:19 +0300

    'Easy'

---
+++ grading.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    ref=[]
+    for i in grades:
+        if i>=38:
+            temp=i%5
+            if temp>=3:
+                i+=(5-temp)
+        ref.append(i)
+    return ref
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
